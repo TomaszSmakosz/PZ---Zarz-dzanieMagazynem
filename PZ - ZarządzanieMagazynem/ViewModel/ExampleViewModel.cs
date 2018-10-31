d084f47e3c617e8cb39788c7c7bcffb39f657325
Rafał Postek <rpostek94@gmail.com>
2018-10-31 18:04:55 +0100

    struktura mvvm

---
+++ PZ - ZarządzanieMagazynem/ViewModel/ExampleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ___ZarządzanieMagazynem.ViewModel
+{
+    internal class ExampleViewModel
+    {
+        //todo tutki z MVVM i caliburn.micro
+    }
+}
