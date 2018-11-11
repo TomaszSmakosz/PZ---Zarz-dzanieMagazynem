c4fc7750d40e365ac7cdad7e7d8ff92a721a2a66
SmugSkeletor <wakemeupinside7@gmail.com>
2018-11-11 14:50:00 +0100

    Dodanie widoku dodawania klienta oraz ViewModel mu towarzyszący

---
+++ PZ - ZarządzanieMagazynem/ViewModel/AddClientViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ___ZarządzanieMagazynem.ViewModel
+{
+    //ViewModel do widoku AddClientView
+    internal class AddClientViewModel
+    {
+    }
+}
