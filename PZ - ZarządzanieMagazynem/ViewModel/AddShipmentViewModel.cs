fa41086f7d2377545014b65121fe2b44b550fdb2
SmugSkeletor <wakemeupinside7@gmail.com>
2018-11-26 17:04:30 +0100

    Dodanie widoku i ViewModel AddShipment

---
+++ PZ - ZarządzanieMagazynem/ViewModel/AddShipmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ___ZarządzanieMagazynem.ViewModel
+{
+    //ViewModel do widoku AddShipment
+    public class AddShipmentViewModel : Screen
+    {
+    }
+}
