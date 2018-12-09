43627f57437998a57e47ec5d3e1e2e743bfc6bb2
gborowy <borek1709@gmail.com>
2018-12-09 20:01:36 +0100

    Dodanie serwisów dla usera i equipmenta

---
+++ WarehouseSystem/Service/UserService.cs
@@ -0,0 +1,135 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseSystem.DTO;
+using WarehouseSystem.Models;
+
+namespace WarehouseSystem.Service
+{
+    public class UsertService
+    {
+        public static List<UserDTO> GetAll()
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                var result = db.Users.Where(x => x.IsDisabled == false).Select(
+                                   x => new UserDTO
+                                   {
+                                       Id = x.Id,
+                                       FirstName = x.FirstName,
+                                       LastName = x.LastName,
+                                       Email = x.Email,
+                                       PhoneNumber = x.PhoneNumber,
+                                       BirthDate = x.BirthDate,
+                                   }).ToList();
+                return result;
+            }
+        }
+
+        public static BindableCollection<UserDTO> GetAllBindableCollection()
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                var result = new BindableCollection<UserDTO>(GetAll());
+                return result;
+            }
+        }
+
+        public static UserDTO GetById(int id)
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                var result = db.Users.Where(x => x.Id == id).Select(
+                                    x => new UserDTO
+                                    {
+                                        Id = x.Id,
+                                        FirstName = x.FirstName,
+                                        LastName = x.LastName,
+                                        Email = x.Email,
+                                        PhoneNumber = x.PhoneNumber,
+                                        BirthDate = x.BirthDate,
+                                    }).FirstOrDefault();
+
+                return result;
+            }
+        }
+
+        public static string Add(UserDTO user)
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                string error = null;
+                User newUser = new User();
+                newUser.Id = user.Id;
+                newUser.FirstName = user.FirstName;
+                newUser.LastName = user.LastName;
+                newUser.Email = user.Email;
+                newUser.PhoneNumber = user.PhoneNumber;
+                newUser.BirthDate = user.BirthDate;
+
+                var context = new ValidationContext(newUser, null, null);
+                var result = new List<ValidationResult>();
+                Validator.TryValidateObject(newUser, context, result, true);
+
+                foreach (var x in result)
+                {
+                    error = error + x.ErrorMessage + "\n";
+                }
+
+                if (error == null)
+                {
+                    db.Users.Add(newUser);
+                    db.SaveChanges();
+                }
+                return error;
+            }
+        }
+
+        public static string Edit(UserDTO user)
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                string error = null;
+
+                var toModify = db.Users.Where(x => x.Id == user.Id).FirstOrDefault();
+
+                toModify.Id = user.Id;
+                toModify.FirstName = user.FirstName;
+                toModify.LastName = user.LastName;
+                toModify.Email = user.Email;
+                toModify.PhoneNumber = user.PhoneNumber;
+                toModify.BirthDate = user.BirthDate;
+
+                var context = new ValidationContext(toModify, null, null);
+                var result = new List<ValidationResult>();
+                Validator.TryValidateObject(toModify, context, result, true);
+
+                foreach (var x in result)
+                {
+                    error = error + x.ErrorMessage + "\n";
+                }
+
+                if (error == null)
+                {
+                    db.SaveChanges();
+                }
+                return error;
+            }
+        }
+
+        public static void Delete(UserDTO user)
+        {
+            using (WarehouseContext db = new WarehouseContext())
+            {
+                var toDelete = db.Users.Where(x => x.Id == user.Id).FirstOrDefault();
+                toDelete.IsDisabled = true;
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
