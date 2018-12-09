940aadb1bf761355b3ac424a38c8f1ce9c9ee7bb
SmugSkeletor <wakemeupinside7@gmail.com>
2018-12-09 18:18:28 +0100

    model Shipment

--- WarehouseSystem/Model/Shipment.cs
+++ WarehouseSystem/Model/Shipment.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WarehouseSystem.Model
 {
+    [Table("Shipment")]
     public class Shipment
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ShippedItem { get; set; }
+
+        [Required]
+        public int ItemQuantity { get; set; }
+
+        [Required]
+        public string RecipientCompany { get; set; }
+
+        [Required]
+        public string CityTown { get; set; }
+
+        [Required]
+        public int PostalCode { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        public bool IsDisable { get; set; }
     }
 }
