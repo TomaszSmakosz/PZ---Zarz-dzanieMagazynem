bea3b6cc099cadfa42c7ca99ea4631ecf1a832f7
Tomo Omot <flameoking@gmail.com>
2019-01-15 15:54:40 +0100

    inventory

--- WarehouseSystem/ViewModels/AddOrderViewModels.cs
+++ WarehouseSystem/ViewModels/AddOrderViewModels.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WarehouseSystem.DTO;
+using WarehouseSystem.Service;
 
 namespace WarehouseSystem.ViewModels
 {
