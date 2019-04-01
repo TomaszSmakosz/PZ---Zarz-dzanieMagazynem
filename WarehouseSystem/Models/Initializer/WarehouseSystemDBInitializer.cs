fa8917311419c6753eda354723fb5902d034801d
gborowy <borek1709@gmail.com>
2019-04-01 17:53:49 +0200

    tu dodac seeda

---
+++ WarehouseSystem/Models/Initializer/WarehouseSystemDBInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseSystem.Models.Initializer
+{
+    internal class WarehouseSystemDBInitializer
+    {
+        public static void Seed(WarehouseSystemContext db)
+        {
+        }
+    }
+}
