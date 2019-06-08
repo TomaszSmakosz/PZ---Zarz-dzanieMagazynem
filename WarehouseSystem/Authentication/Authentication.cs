9bf113ea462d186aaee95d209182a2606ca48617
SmugSkeletor <wakemeupinside7@gmail.com>
2019-06-08 19:00:06 +0200

    Dodanie logowania (admin admin)

---
+++ WarehouseSystem/Authentication/Authentication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseSystem.Authentication
+{
+    public class Authentication
+    {
+        public static string Username { get; set; }
+        public static bool isLogged { get; set; }
+        public static bool isAdmin { get; set; }
+
+        public Authentication()
+        {
+            Username = string.Empty;
+            isLogged = false;
+            isAdmin = false;
+        }
+    }
+}
--- WarehouseSystem/ViewModels/User/LogInViewModel.cs
+++ WarehouseSystem/ViewModels/User/LogInViewModel.cs
@@ -3,16 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security;
 using System.Threading.Tasks;
 using WarehouseSystem.DTO;
 using WarehouseSystem.Service;
+using WarehouseSystem.Authentication;
 
-namespace WarehouseSystem.ViewModels.User
+namespace WarehouseSystem.ViewModels
 {
     public class LogInViewModel : Screen
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
         public LogInViewModel()
         {
+        }
+
+        public void LogIn(string username, string password)
+        {
+            if (Username == "admin" && Password == "admin")
+            {
+                Authentication.Authentication.Username = "admin";
+                Authentication.Authentication.isLogged = true;
+                Authentication.Authentication.isAdmin = true;
+                TryClose();
+            }
         }
 
         public void Close()
